ee01a35b5d9012d121c2c70acfa74b11afc319c8
daviiiXD <90698393+daviiiXD@users.noreply.github.com>
2022-06-09 12:38:50 -0500

    Primeira at
    Quanto rgb.....

---
+++ dad/mian.js
@@ -0,0 +1,10 @@
+let color = [
+    "red",
+    "green",
+    "blue"
+]
+let count = 0
+
+for (let i = 0; i < 3; i ++) {
+    document.getElementById('container').innerHTML += "<div class=\"card\" id=\"card\"><img class=\"icon\" src=\"https://preview.redd.it/nlkzl08jfr171.jpg?width=640&crop=smart&auto=webp&s=ed9eb18fce791cc7aace2f59e117c1059b99b039\"><br/><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio voluptates debitis placeat, ipsum ex dignissimos modi reprehenderit magni praesentium commodi id animi odit nostrum soluta repellendus accusamus ipsa repudiandae maxime.</p></div>"
+}
